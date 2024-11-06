8a9073634537f7b18d9dd75db83af13014a94dc8
ZEVS1206 <egor_bud5@inbox.ru>
2024-11-06 13:59:17 +0300

    Did insert after element in list

---
+++ include/list.h
@@ -0,0 +1,41 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef int List_Elem_t;
+
+const size_t size_of_list = 10;
+
+enum Errors_of_list
+{
+    NO_ERRORS            = 0,
+    ERROR_OF_PUSH        = 1,
+    ERROR_OF_POP         = 2,
+    ERROR_OF_INDEX       = 3,
+    ERROR_OF_CONSTRUCTOR = 4,
+    ERROR_OF_DESTRUCTOR  = 5,
+    ERROR_OF_PRINT       = 6,
+    ERROR_OF_NO_ELEMENT  = 7
+};
+
+struct Data
+{
+    List_Elem_t element;
+    int next_index;
+    int prev_index;
+};
+
+struct MyList
+{
+    Data *data;
+    size_t size_of_list;
+    int head;
+    int tail;
+};
+
+Errors_of_list list_push(struct MyList *list, int index, List_Elem_t elem);
+Errors_of_list list_pop(struct MyList *list, int index, List_Elem_t *elem);
+Errors_of_list list_check(struct MyList *list);
+Errors_of_list list_print(struct MyList *list);
+
+
+#endif
---
+++ source/list_functions.cpp
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+Errors_of_list list_print(struct MyList *list)
+{
+    if (list == NULL)
+    {
+        return ERROR_OF_PRINT;
+    }
+    printf("data: ");
+    int j = 1;
+    while (j != -1)
+    {
+        // printf("\n");
+        // for (size_t i = 1; i < (list->size_of_list); i++)
+        // {
+        //     printf("%2d ", (list->data)[i].next_index);
+        // }
+        // printf("\n");
+        // for (size_t i = 1; i < (list->size_of_list); i++)
+        // {
+        //     printf("%2lu ", i);
+        // }
+        // printf("\n");
+        // getchar();
+        //printf("j = %d next_index = %d\n", j, (list->data)[j].next_index);
+        if ((list->data)[j].next_index == -1)
+        {
+            break;
+        }
+        printf("%d ", (list->data)[j].element);
+        j = (list->data)[j].next_index;
+    }
+    printf("\n");
+    // printf("%d ", (list->data)[1].element);
+    // for (size_t i = 1; i < (list->size_of_list); i++)
+    // {
+    //     if ((list->data)[i].next_index == -1)
+    //     {
+    //         continue;
+    //     }
+    //     //printf("i = %lu\n", i);
+    //     printf("%d ", (list->data)[(list->data)[i].next_index].element);
+    // }
+    printf("\nnext: ");
+    for (size_t i = 1; i < (list->size_of_list); i++)
+    {
+        printf("%2d ", (list->data)[i].next_index);
+    }
+    printf("\n");
+    // printf("\nprev: ");
+    // for (size_t i = 1; i < (list->size_of_list); i++)
+    // {
+    //     printf("%d ", (list->data)[i].prev_index);
+    // }
+    // printf("\n");
+    return NO_ERRORS;
+}
+
+Errors_of_list list_push(struct MyList *list, int index, List_Elem_t elem)
+{
+    if (list == NULL || index < 0 || index > (list->size_of_list))
+    {
+        return ERROR_OF_PUSH;
+    }
+    if (list->head == 0)
+    {
+        (list->data)[index + 1].element = elem;
+        (list->data)[index + 1].next_index = index + 2;
+        (list->data)[index + 1].prev_index = index;
+        (list->head)++;
+        (list->tail)++;
+        return NO_ERRORS;
+    }
+    int free_index = -1;
+    int flag = 0;
+    if ((list->data)[index + 1].next_index == -1)
+    {
+        free_index = index + 1;
+        (list->data)[free_index].element = elem;
+        (list->data)[free_index].next_index = index + 2;
+    }
+    else
+    {
+        for (int i = 0; i < (list->size_of_list); i++)
+        {
+            if (flag == 0 && (list->data)[i].next_index == -1)
+            {
+                flag = 1;
+            }
+            else if ((list->data)[i].next_index == -1)
+            {
+                free_index = i;
+                break;
+            }
+        }
+        (list->data)[free_index].element = elem;
+        (list->data)[free_index].next_index = (list->data)[index + 1].next_index;
+        (list->data)[index + 1].next_index = free_index;
+    }
+    //printf("free_index = %d\n", free_index);
+    if (free_index == -1)
+    {
+        return ERROR_OF_NO_ELEMENT;
+    }
+    (list->tail)++;
+    return NO_ERRORS;
+}
+
